6a79df50825634d694eaa73deadd78b6a1449f6a
Hanen Wechteti <hanenwechteti27@gmail.com>
2024-01-04 14:57:31 +0100

    exercices

---
+++ excercies/script.js
@@ -0,0 +1,36 @@
+let index = 0;
+let display = document.getElementById('display');
+function typeWriter() {
+    let word = 'Wilson3';
+    let span = document.createElement('span');
+    span.innerHTML = word[index];
+    display.appendChild(span);
+       // Trigger a reflow to apply the transition effect
+      span.offsetWidth;
+
+       span.style.opacity = 1;
+    index++;
+    if (index == word.length){
+        clearInterval(intervalBetween);
+    }
+}
+let intervalBetween =  setInterval(typeWriter, 1000);
+
+let displayElapsedTime = document.createElement('div');
+document.body.appendChild(displayElapsedTime);
+let elapsedTime = 0;
+let span = document.createElement('span');
+displayElapsedTime.appendChild(span);
+
+function updateElapsedTime() {
+    if( elapsedTime / 60 === 1) {
+        span.textContent = 'elapsedTime : ' + elapsedTime / 60 + 'minute(s)';
+    }
+    else if( elapsedTime % 60 === 0) {
+            span.textContent = 'elapsedTime : ' + elapsedTime / 60 + 'minute(s)';
+        }
+
+    elapsedTime++;
+}
+
+setInterval(updateElapsedTime, 1000);
